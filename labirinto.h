f33a7b20d80c7d3d6546e5476d72f1bc8830f8c7
HiagoFreitas0 <pudhhdhdy@gmail.com>
2025-05-20 18:53:19 -0300

    Update labirinto.h

--- labirinto.h
+++ labirinto.h
@@ -9,55 +9,62 @@ typedef struct individuo Individuo;
 typedef struct populacao Populacao;
 
 /**
- * Cria e inicializa uma população de indivíduos.
- * @return Ponteiro para a população criada.
+ * Cria e inicializa uma população de indivíduos
+ * @return Ponteiro para a população criada
  */
 Populacao *criar_populacao();
 
 /**
- * Inicializa a população com indivíduos aleatórios.
- * @param pop Ponteiro para a população.
+ * Inicializa a população com indivíduos aleatórios
+ * @param pop Ponteiro para a população
  */
 void inicializar_populacao(Populacao *pop);
 
 /**
- * Avalia o fitness de cada indivíduo da população.
- * @param pop Ponteiro para a população.
- */
-void avaliar_populacao(Populacao *pop);
-
-/**
- * Calcula o fitness de um indivíduo com base em seus movimentos.
- * @param movimentos Sequência de movimentos do indivíduo.
- * @return Fitness calculado.
+ * Calcula o fitness de um indivíduo com base em seus movimentos
+ * @param movimentos Sequência de movimentos do indivíduo
+ * @return Fitness calculado
  */
 int calcular_fitness(char *movimentos);
 
 /**
- * Retorna o melhor indivíduo da população.
- * @param pop Ponteiro para a população.
- * @return Ponteiro para o melhor indivíduo.
+ * Avalia o fitness de cada indivíduo da população
+ * @param pop Ponteiro para a população
+ */
+void avaliar_populacao(Populacao *pop);
+
+/**
+ * Retorna o melhor indivíduo da população
+ * @param pop Ponteiro para a população
+ * @return Ponteiro para o melhor indivíduo
  */
 Individuo *melhor_individuo(Populacao *pop);
 
 /**
- * Libera a memória alocada para a população e seus indivíduos.
- * @param pop Ponteiro para a população.
+ * Libera a memória alocada para a população e seus indivíduos
+ * @param pop Ponteiro para a população
  */
 void liberar_populacao(Populacao *pop);
 
 /**
- * Retorna o fitness de um indivíduo.
- * @param ind Ponteiro para o indivíduo.
- * @return Valor do fitness.
+ * Retorna o fitness de um indivíduo
+ * @param ind Ponteiro para o indivíduo
+ * @return Valor do fitness
  */
 int obter_fitness(const Individuo *ind);
 
 /**
- * Retorna a sequência de movimentos de um indivíduo.
- * @param ind Ponteiro para o indivíduo.
- * @return Ponteiro para a sequência de movimentos.
+ * Retorna a sequência de movimentos de um indivíduo
+ * @param ind Ponteiro para o indivíduo
+ * @return Ponteiro para a sequência de movimentos
  */
 const char* obter_movimentos(const Individuo *ind);
+
+/**
+ * Imprime a sequência de movimentos de um indivíduo no formato de lista
+ * Exemplo de saída: [D, E, B, C, D]
+ * @param movimentos Ponteiro para a string que contém os movimentos (como 'D', 'E', 'B', 'C') terminada por '\0'
+ */
+void imprimir_caminho(const char *movimentos);
 
 #endif
